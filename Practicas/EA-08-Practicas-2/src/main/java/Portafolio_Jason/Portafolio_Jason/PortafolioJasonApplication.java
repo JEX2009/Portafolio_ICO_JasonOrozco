aa3f8f2436035d336ce1788e9349ba59c8eac227
Jason <152582224+JEX2009@users.noreply.github.com>
2025-02-01 20:53:03 -0600

    Se crea una carpeta para practicas

---
+++ Practicas/EA-08-Practicas-2/src/main/java/Portafolio_Jason/Portafolio_Jason/PortafolioJasonApplication.java
@@ -0,0 +1,13 @@
+package Portafolio_Jason.Portafolio_Jason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioJasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioJasonApplication.class, args);
+	}
+
+}
