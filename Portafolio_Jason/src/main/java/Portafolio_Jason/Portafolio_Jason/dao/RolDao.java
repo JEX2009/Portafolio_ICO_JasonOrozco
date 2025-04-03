3fcd50ef188c71fcd3b28d4d4b808083ac9033f4
Jason <152582224+JEX2009@users.noreply.github.com>
2025-04-02 20:03:40 -0600

    Semana 11

---
+++ Portafolio_Jason/src/main/java/Portafolio_Jason/Portafolio_Jason/dao/RolDao.java
@@ -0,0 +1,7 @@
+package Portafolio_Jason.Portafolio_Jason.dao;
+import Portafolio_Jason.Portafolio_Jason.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
